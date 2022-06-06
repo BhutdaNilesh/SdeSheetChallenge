41606862724c4710194e4ca9baba45a7a2d69a04
Nilesh Bhutda <bhutdanilesh2517@gmail.com>
2022-06-06 09:59:13 +0530

    BFS using Matrix

--- Graph/BFS.cpp
+++ Graph/BFS.cpp
@@ -38,6 +38,47 @@ vector<int> BFS(int V, vector<pair<int, int>> edges)
     return ans;
 }
 
+
+//Using Matrix
+
+// #include<bits/stdc++.h>
+
+// vector<int> BFS(int V, vector<pair<int, int>> edges)
+// {
+//     // Write your code here
+//     vector<vector<int>>mat(V, vector<int>(V,0));
+//     for(auto it: edges){
+//         int u = it.first;
+//         int v = it.second;
+//         mat[u][v]=1;
+//         mat[v][u]=1;
+//     }
+//     vector<int>vis(V,0);
+//     vector<int>ans;
+//     for(int i=0;i<V;i++){
+//         if(!vis[i]){
+//             queue<int>q;
+//             q.push(i);
+//             vis[i]=1;
+//             while(!q.empty()){
+//                 int node = q.front();
+//                 q.pop();
+//                 ans.push_back(node);
+//                 for(int j=0;j<V;j++){
+//                     if(mat[node][j]==1 && vis[j]==0){
+//                         q.push(j);
+//                         vis[j]=1;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+
+//     return ans;
+// }
+
+
+
 // #include<bits/stdc++.h>
 
 // vector<int> BFS(int V, vector<pair<int, int>> edges)
