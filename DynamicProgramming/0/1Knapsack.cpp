0f362b9def41b37c7a5c94a7e30682f9452d2b7f
Nilesh Bhutda <bhutdanilesh2517@gmail.com>
2022-06-06 23:05:18 +0530

    0 1 Knapsack tabulation

--- DynamicProgramming/0/1Knapsack.cpp
+++ DynamicProgramming/0/1Knapsack.cpp
@@ -19,3 +19,28 @@ int maxProfit(vector<int> &values, vector<int> &weights, int n, int W)
     vector<vector<int>>dp(n, vector<int>(W+1,-1));
     return solve(n-1,W, values, weights,dp);
 }
+
+
+//Tabulation
+
+int maxProfit(vector<int> &val, vector<int> &wt, int n, int maxW)
+{
+	// Write your code here
+    vector<vector<int>>dp(n, vector<int>(maxW+1,0));
+    
+    for(int W =wt[0];W<maxW;W++){
+        dp[0][W] = val[0];
+    }
+    
+    for(int ind=1;ind<n;ind++){
+        for(int W=0;W<=maxW;W++){
+            int notTake = 0 + solve(ind-1, W, val, wt,dp);
+            int take = INT_MIN;
+            if(wt[ind]<=W){
+                take = val[ind] + solve(ind-1, W-wt[ind], val, wt,dp);
+            }
+            dp[ind][W] = max(take, notTake);
+        }
+    }
+    return dp[n-1][maxW];
+}
