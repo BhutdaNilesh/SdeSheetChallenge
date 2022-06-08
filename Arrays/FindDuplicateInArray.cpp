4b8cc4e00a902c90e009af67a401851c49a8d7d7
Nilesh Bhutda <bhutdanilesh2517@gmail.com>
2022-06-08 09:53:16 +0530

    Find Duplicate in Array Tortoise method

--- Arrays/FindDuplicateInArray.cpp
+++ Arrays/FindDuplicateInArray.cpp
@@ -14,3 +14,25 @@ int findDuplicate(vector<int> &arr, int n){
     }
     return res;
 }
+
+
+// Method 2 Tortoise method
+
+#include <bits/stdc++.h> 
+int findDuplicate(vector<int> &nums, int n){
+	// Write your code here.
+    int slow = nums[0];
+    int fast = nums[0];
+    
+    do{
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    }while(slow!=fast);
+    
+    fast = nums[0];
+    while(slow!=fast){
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+    return slow;
+}
