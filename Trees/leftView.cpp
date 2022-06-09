8152783fadcc7d28ace4e06ba2adefd40125399f
Nilesh Bhutda <bhutdanilesh2517@gmail.com>
2022-06-09 11:22:05 +0530

    Left View Of a Binary Tree

---
+++ Trees/leftView.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+void leftView(TreeNode<int>* root, int level, vector<int>& ans){
+    if(!root) return;
+    
+    if(ans.size()==level) ans.push_back(root->data);
+    leftView(root->left, level+1, ans);
+    leftView(root->right, level+1, ans);
+}
+
+vector<int> getLeftView(TreeNode<int> *root)
+{
+    //    Write your code here
+    vector<int>ans;
+    if(!root) return ans;
+    leftView(root,0,ans);
+    return ans;
+}
