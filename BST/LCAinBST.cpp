2affd71d6997f8e9543e31c7854bcf44679b85af
Nilesh Bhutda <bhutdanilesh2517@gmail.com>
2022-06-11 10:36:19 +0530

    Binary Search Tree

---
+++ BST/LCAinBST.cpp
@@ -0,0 +1,44 @@
+/************************************************************
+    Following is the Binary Search Tree node structure
+    
+    template <typename T>
+    class TreeNode {
+        public :
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~TreeNode() {
+            if(left)
+                delete left;
+            if(right)
+                delete right;
+        }
+    };
+
+************************************************************/
+
+TreeNode<int>* LCAinaBST(TreeNode<int>* root, TreeNode<int>* P, TreeNode<int>* Q)
+{
+	// Write your code here
+    if(root==NULL || root==P || root==Q){
+        return root;
+    }
+    if(root->data > P->data && root->data > Q->data){
+        TreeNode<int>* left = LCAinaBST(root->left, P,Q);
+        return left;
+    }
+    else if(root->data< P->data && root->data < Q->data){
+        TreeNode<int>* right = LCAinaBST(root->right,P,Q);
+        return right;
+    }
+    
+    
+    return root;
+}
