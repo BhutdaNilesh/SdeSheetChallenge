39afcd1d16fddeb6f74920cfdabea2f785d9efb6
Nilesh Bhutda <bhutdanilesh2517@gmail.com>
2022-06-14 19:19:00 +0530

    Merge Two Sorted Linked Lists

--- LinkedList/merge2SortedLinkedList.cpp
+++ LinkedList/merge2SortedLinkedList.cpp
@@ -53,3 +53,48 @@ Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
     
     return dummy->next;
 }
+
+
+
+// Better Approcach
+
+
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the linked list node structure.
+    
+    template <typename T>
+    class Node {
+        public:
+        T data;
+        Node* next;
+
+        Node(T data) {
+            next = NULL;
+            this->data = data;
+        }
+
+        ~Node() {
+            if (next != NULL) {
+                delete next;
+            }
+        }
+    };
+
+************************************************************/
+
+Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
+{
+    // Write your code here.
+    if(first==NULL) return second;
+    if(second==NULL) return first;
+    
+    if(first->data<second->data){
+        first->next = sortTwoLists(first->next,second);
+        return first;
+    }else{
+        second->next = sortTwoLists(second->next,first);
+        return second;
+    }
+}
