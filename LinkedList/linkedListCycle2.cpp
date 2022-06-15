87b34f125e396fa8f3203933423bc0d3075702ad
Nilesh Bhutda <bhutdanilesh2517@gmail.com>
2022-06-15 15:57:08 +0530

    Linked List Cycle II

--- LinkedList/linkedListCycle2.cpp
+++ LinkedList/linkedListCycle2.cpp
@@ -31,3 +31,48 @@ Node *firstNode(Node *head)
     }
     return NULL;
 }
+
+
+
+// Method 2:
+
+#include <bits/stdc++.h> 
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+Node *firstNode(Node *head)
+{
+	//    Write your code here.
+    Node* slow = head, *fast = head;
+    
+    if(head==NULL || head->next==NULL) return NULL;
+    
+    while(fast && fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow==fast){
+            fast = head;
+            while(slow!=fast){
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return slow;
+        }
+    }
+    return NULL;
+}
