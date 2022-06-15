be434235e2e197315655413e734798edfd346743
Nilesh Bhutda <bhutdanilesh2517@gmail.com>
2022-06-15 16:02:38 +0530

    Partial BST

--- BST/partialBST.cpp
+++ BST/partialBST.cpp
@@ -38,3 +38,43 @@ bool validateBST(BinaryTreeNode<int> *root) {
     return true;
     
 }
+
+
+
+// Method 2:
+
+/*************************************************************
+ 
+    Following is the Binary Tree node structure
+
+    class BinaryTreeNode 
+    {
+    public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
+
+bool validateBST(BinaryTreeNode<int> *root) {
+    // Write your code here
+    
+    if(!root) return true;
+    
+    if(root->left){
+        if(root->data < root->left->data) return false;
+    }
+    if(root->right){
+        if(root->data > root->right->data) return false;
+        
+    }
+    
+    return validateBST(root->left) && validateBST(root->right);
+}
