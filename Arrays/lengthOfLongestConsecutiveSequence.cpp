ba0c2ee9ac05b3ba414c8b4f2c7f39c48088e45f
Nilesh Bhutda <bhutdanilesh2517@gmail.com>
2022-06-18 16:49:20 +0530

    Longest Consecutive Sequence

--- Arrays/lengthOfLongestConsecutiveSequence.cpp
+++ Arrays/lengthOfLongestConsecutiveSequence.cpp
@@ -18,3 +18,33 @@ int lengthOfLongestConsecutiveSequence(vector<int> &arr, int n) {
     }
     return max(maxi, cnt);
 }
+
+
+
+
+
+//
+
+#include <bits/stdc++.h> 
+int lengthOfLongestConsecutiveSequence(vector<int> &arr, int n) {
+    // Write your code here.
+    
+    map<int,int>mp;
+    for(auto it: arr){
+        mp[it]++;
+    }
+    int maxi=0;
+    for(auto it: arr){
+        if(mp.find(it-1)==mp.end()){
+            int curr= it;
+            int cnt=1;
+            while(mp.find(curr+1)!=mp.end()){
+                curr++;
+                cnt++;
+            }
+            
+            maxi = max(maxi, cnt);
+        }
+    }
+    return maxi;
+}
