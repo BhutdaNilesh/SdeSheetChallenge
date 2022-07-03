b3df2b17529c1f0e12c577ce662a8b501298d689
Nilesh Bhutda <bhutdanilesh2517@gmail.com>
2022-07-03 09:00:53 +0530

    Maximum In Sliding Windows Of Size K

---
+++ SlidingWindow/slidingWindowMaximum.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+vector<int> slidingWindowMaximum(vector<int> &nums, int &k)
+{
+    //    Write your code here.
+    priority_queue<pair<int,int>>pq;
+        
+    vector<int>ans;
+
+    for(int i=0;i<k;i++){
+        pq.push({nums[i],i});
+    }
+    ans.push_back(pq.top().first);
+
+    for(int i=k;i<nums.size();i++){
+        pq.push({nums[i],i});
+
+        while(!pq.empty() && pq.top().second<=i-k){
+
+            pq.pop();
+        }
+        ans.push_back(pq.top().first);
+    }
+    return ans;
+}
