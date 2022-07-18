76ae8bb6279e92f0d7e5c8f6439d89728239bc81
Nilesh Bhutda <bhutdanilesh2517@gmail.com>
2022-07-18 17:25:29 +0530

    Maximum XOR of Two Numbers in an Array

---
+++ Trie/maxXORofTwoNumbersInArray.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+int maximumXor(vector<int> nums)
+{
+    // Write your code here.   
+    int ans = 0 ; int prefix = 0 ;
+    for(int i = 31; i>= 0 ;i--){
+        prefix = prefix | (1<< i);
+        unordered_set< int> s; 
+
+        for(auto it : nums){
+            s.insert(it & prefix) ; 
+        }
+        int c = ans| (1 << i);
+        for(auto e : s){
+            int b = e^c ; 
+            if(s.find(b)!= s.end()){
+                ans = c; 
+                break ;
+            }
+        }
+    }
+        
+    return ans;
+
+}
